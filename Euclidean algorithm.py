08f37458b3a0b83bd69c9cf2e27a804214ee5c8b
Mohammad SN <shahnazi2002@gmail.com>
2021-11-21 23:41:43 +0330

    Update Euclidean algorithm.py

--- Euclidean algorithm.py
+++ Euclidean algorithm.py
@@ -3,12 +3,8 @@ b = int(input("Enter the second number: "))
 if a > b:
     while b > 0:
         a, b = b, a % b
-        if b == 0:
-            break
 elif b > a:
     a, b = b, a
     while b > 0:
         a, b = b, a % b
-        if b == 0:
-            break
 print("GCD =", a)
